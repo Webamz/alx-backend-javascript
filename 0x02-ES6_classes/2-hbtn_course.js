2bd8fd7e87c32bcb3ca548f500aeb9c8fd33f951
Webamz <chriszxn99@gmail.com>
2023-09-28 15:48:08 +0200

    codestyle

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,37 +1,37 @@
 export default class HolbertonCourse {
-  constructor (name, length, students) {
+  constructor(name, length, students) {
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  get name () {
+  get name() {
     return this._name;
   }
 
-  set name (name) {
+  set name(name) {
     if ((typeof name !== 'string') && !(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
-  get length () {
+  get length() {
     return this._length;
   }
 
-  set length (length) {
+  set length(length) {
     if ((typeof length !== 'number') && !(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
-  get students () {
+  get students() {
     return this._students;
   }
 
-  set students (students) {
+  set students(students) {
     if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
